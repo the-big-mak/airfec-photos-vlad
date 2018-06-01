85185755624acb58b0b6d177484fd59ea4d18242
Poisk <vpisotski@gmail.com>
2018-06-01 16:27:32 -0700

    populated database with dummydata. Getting valid urls from s3 server.

--- helpers/getPhotos.js
+++ helpers/getPhotos.js
@@ -1,13 +1,18 @@
-const request = require('request');
 const config = require('./config.js');
 const AWS = require('aws-sdk');
+
 AWS.config.update({
   accessKeyId: config.header.accessKeyId,
   secretAccessKey: config.header.secretAccessKey,
   region: 'us-east-2'
 });
 
-const getPhotos = () => {
+
+const transformer = (array) => {
+  return array.map((img) => 'https://s3.us-east-2.amazonaws.com/airfec/' + img.Key);
+}
+
+const getPhotos = (callback) => {
 
   const bucketParams = {
      Bucket: 'airfec'
@@ -17,9 +22,11 @@ const getPhotos = () => {
                                      
   s3.listObjects(bucketParams, function(err, data) {
      if (err) {
+        callback(transformer(data.Contents), null)
         console.log("Error", err);
      } else {
-        console.log("Success", data);
+        callback(null, transformer(data.Contents))
+        console.log("Success");
      }
   });
 }
--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
-const db = require('./connection.js')
+const db = require('./connection.js');
+const helper = require('../helpers/getPhotos.js')
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/photos', (req, res) => {
   var sql = 'SELECT * FROM photos';
-  db.getPhotos(sql, (err, data) => {
+  db.getPhotos(sql, (err, data) => 
     err ? res.status(500).send() : res.status(200).send(data)
-  })
+  )
 });
+
+app.get('/getPhotos', (req, res) => {
+  helper.getPhotos((err, data) => 
+    err ? res.status(500).send() : res.status(200).send(data)
+    );
+})
 
 var PORT = PORT || 3001;
 app.listen(PORT, () => console.log(`Photos module listening on port ${PORT}!`));
