40f4af1ef15526e4ef66c172dca3e80da0e48776
Poisk <vpisotski@gmail.com>
2018-06-08 13:19:23 -0700

    fixed bug when only 6 photos where renderred

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -52,7 +52,7 @@ class Carousel extends React.Component {
     if (currentIndex < 4) {
       return collection.slice(0, 7);
     }
-    return currentIndex > collection.length - 3 ? collection.slice(collection.length - 7) : collection.slice(currentIndex - 3, currentIndex + 3);
+    return currentIndex > collection.length - 4 ? collection.slice(collection.length - 7) : collection.slice(currentIndex - 3, currentIndex + 4);
   };
   previousSlide() {
     const imgUrls = this.props.collection;
