91c3b35ccd6ea0d943f97bd6b2ba462e578b201f
Poisk <vpisotski@gmail.com>
2018-06-13 20:04:00 -0700

    bundle

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-// const axios = require('axios');
+const cors = require('cors');
 const path = require('path');
 const db = require('./connection.js');
 // const helper = require('../helpers/getPhotos.js');
 const PORT = process.env.PORT || 3003;
 
 const app = express();
-
+app.use(cors());
 app.use('/rooms/:id', express.static(path.join(__dirname, '../public/dist')));
 
-app.get('/Photos/:roomnumber', (req, res) => {
-  const sql = `SELECT * FROM photos WHERE roomid = ${req.params.roomnumber}`;
+app.get('/Photos/:id', (req, res) => {
+  const sql = `SELECT * FROM photos WHERE roomid = ${req.params.id}`;
   db.dbManipulator(sql, (err, data) => (err ? res.status(500).send() : res.status(200).send(data)));
 });
 
